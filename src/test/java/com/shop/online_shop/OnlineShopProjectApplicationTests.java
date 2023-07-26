f2f0293821e767dbc741596429110a07f05469d6
lgakan <lgakan@student.agh.edu.pl>
2023-07-26 20:47:02 +0200

    working initial

---
+++ src/test/java/com/shop/online_shop/OnlineShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
